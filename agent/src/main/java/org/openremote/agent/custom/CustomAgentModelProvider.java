aa23d1681cb808c1a4f9995f1a02fe43eed1de35
Rich Turner <7072278+richturner@users.noreply.github.com>
2023-11-09 12:27:40 +0000

    Updated to latest openremote submodule

--- agent/src/main/java/org/openremote/agent/custom/CustomAgentModelProvider.java
+++ agent/src/main/java/org/openremote/agent/custom/CustomAgentModelProvider.java
@@ -21,44 +21,11 @@
 package org.openremote.agent.custom;
 
 import org.openremote.model.AssetModelProvider;
-import org.openremote.model.asset.Asset;
-import org.openremote.model.asset.AssetDescriptor;
-import org.openremote.model.value.AttributeDescriptor;
-import org.openremote.model.value.MetaItemDescriptor;
-import org.openremote.model.value.ValueDescriptor;
-
-import java.util.List;
-import java.util.Map;
 
 public class CustomAgentModelProvider implements AssetModelProvider {
 
     @Override
     public boolean useAutoScan() {
         return true;
-    }
-
-    @Override
-    public AssetDescriptor<?>[] getAssetDescriptors() {
-        return new AssetDescriptor<?>[0];
-    }
-
-    @Override
-    public Map<Class<? extends Asset<?>>, List<AttributeDescriptor<?>>> getAttributeDescriptors() {
-        return null;
-    }
-
-    @Override
-    public Map<Class<? extends Asset<?>>, List<MetaItemDescriptor<?>>> getMetaItemDescriptors() {
-        return null;
-    }
-
-    @Override
-    public Map<Class<? extends Asset<?>>, List<ValueDescriptor<?>>> getValueDescriptors() {
-        return null;
-    }
-
-    @Override
-    public void onAssetModelFinished() {
-
     }
 }
--- model/src/main/java/org/openremote/model/custom/CustomAssetModelProvider.java
+++ model/src/main/java/org/openremote/model/custom/CustomAssetModelProvider.java
@@ -20,44 +20,11 @@
 package org.openremote.model.custom;
 
 import org.openremote.model.AssetModelProvider;
-import org.openremote.model.asset.Asset;
-import org.openremote.model.asset.AssetDescriptor;
-import org.openremote.model.value.AttributeDescriptor;
-import org.openremote.model.value.MetaItemDescriptor;
-import org.openremote.model.value.ValueDescriptor;
-
-import java.util.List;
-import java.util.Map;
 
 public class CustomAssetModelProvider implements AssetModelProvider {
 
     @Override
     public boolean useAutoScan() {
         return true;
-    }
-
-    @Override
-    public AssetDescriptor<?>[] getAssetDescriptors() {
-        return new AssetDescriptor[0];
-    }
-
-    @Override
-    public Map<Class<? extends Asset<?>>, List<AttributeDescriptor<?>>> getAttributeDescriptors() {
-        return null;
-    }
-
-    @Override
-    public Map<Class<? extends Asset<?>>, List<MetaItemDescriptor<?>>> getMetaItemDescriptors() {
-        return null;
-    }
-
-    @Override
-    public Map<Class<? extends Asset<?>>, List<ValueDescriptor<?>>> getValueDescriptors() {
-        return null;
-    }
-
-    @Override
-    public void onAssetModelFinished() {
-
     }
 }
