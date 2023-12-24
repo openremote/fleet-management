9f8bd8acd7c9c15f2f031c6ddaa7c97e16b59940
pankalog <pan.kalog16@gmail.com>
2023-12-24 18:33:39 +0200

    Corrections and adding TeltonikaParameter to CustomValueTypes

--- model/src/main/java/org/openremote/model/custom/CustomValueTypes.java
+++ model/src/main/java/org/openremote/model/custom/CustomValueTypes.java
@@ -1,7 +1,9 @@
 package org.openremote.model.custom;
 
+import org.openremote.model.teltonika.TeltonikaParameter;
 import org.openremote.model.value.ValueDescriptor;
 
 public class CustomValueTypes {
     public static final ValueDescriptor<AssetStateDuration> ASSET_STATE_DURATION = new ValueDescriptor<>("AssetStateDuration", AssetStateDuration.class);
+    public static final ValueDescriptor<TeltonikaParameter> TELTONIKA_PARAMETER = new ValueDescriptor<>("TeltonikaParameter", TeltonikaParameter.class);
 }
