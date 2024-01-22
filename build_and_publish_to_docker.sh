c1aa3911baa4cc0a3b764344e3717f81ebdc9ca8
pankalog <pan.kalog16@gmail.com>
2024-01-22 15:01:07 +0100

    Fleet Management Docker Support
    * docker-compose.yml: Change docker-compose file to reference to pankalog/fleet-management for the manager and pankalog/fleet-deployment for the deployment image
    
    * build_and_publish_to_docker.sh: Made for me to easily rebuild and publish images with 1 script. Takes approximately 4 minutes. Requires login to correct user, repositories create correctly, etc. Publishes to both latest and commit hash tags.

--- build_and_publish_to_docker.sh
+++ build_and_publish_to_docker.sh
@@ -3,6 +3,6 @@
 GIT_DEPLOYMENT_VERSION=$(git rev-parse --short HEAD)
 docker login
 docker buildx create --use --platform=linux/arm64,linux/amd64 --name multi-platform-builder
-docker buildx build --push --platform linux/amd64,linux/arm64 -t pankalog/fleet-deployment:"$GIT_DEPLOYMENT_VERSION" --builder multi-platform-builder  ./deployment/build/
-docker buildx build --push --platform linux/amd64,linux/arm64 -t pankalog/fleet-management:"$GIT_DEPLOYMENT_VERSION" --builder multi-platform-builder  ./openremote/manager/build/install/manager/
+docker buildx build --push --platform linux/amd64,linux/arm64 -t pankalog/fleet-deployment:"$GIT_DEPLOYMENT_VERSION" -t pankalog/fleet-deployment:"latest" --builder multi-platform-builder  ./deployment/build/
+docker buildx build --push --platform linux/amd64,linux/arm64 -t pankalog/fleet-management:"$GIT_DEPLOYMENT_VERSION" -t pankalog/fleet-management:"latest" --builder multi-platform-builder  ./openremote/manager/build/install/manager/
 OR_ADMIN_PASSWORD=secret OR_HOSTNAME=localhost DEPLOYMENT_VERSION="$GIT_DEPLOYMENT_VERSION" MANAGER_VERSION="$GIT_DEPLOYMENT_VERSION" docker-compose -p fleet-management up -d
