529c2984ef17d90c8f0a9d92221f0da79a24f296
pankalog <pan.kalog16@gmail.com>
2024-10-07 12:46:28 +0200

    Update docker-compose for easier versioned deployment

--- build_and_publish_to_docker.sh
+++ build_and_publish_to_docker.sh
@@ -3,6 +3,6 @@
 GIT_DEPLOYMENT_VERSION=$(git rev-parse --short HEAD)
 docker login
 docker buildx create --use --platform=linux/arm64,linux/amd64 --name multi-platform-builder
-docker buildx build --platform linux/amd64,linux/arm64 -t pankalog/fleet-deployment:"$GIT_DEPLOYMENT_VERSION" --builder multi-platform-builder  ./deployment/build/
-docker buildx build --platform linux/amd64,linux/arm64 -t pankalog/fleet-management:"$GIT_DEPLOYMENT_VERSION" --builder multi-platform-builder  ./openremote/manager/build/install/manager/
-docker-compose -p fleet-management up -d
+docker buildx build --load --platform linux/amd64,linux/arm64 -t pankalog/fleet-deployment:"$GIT_DEPLOYMENT_VERSION" --builder multi-platform-builder  ./deployment/build/
+docker buildx build --load --platform linux/amd64,linux/arm64 -t pankalog/fleet-management:"$GIT_DEPLOYMENT_VERSION" --builder multi-platform-builder  ./openremote/manager/build/install/manager/
+DEPLOYMENT_VERSION="$GIT_DEPLOYMENT_VERSION" docker-compose -p fleet-management up -d
